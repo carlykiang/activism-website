2fb7a46cd9062bd330c173a651c2046e54cb2222
Carly Kiang <carlykiang@carlys-air-2.mynetworksettings.com>
2023-05-22 15:48:09 -0400

    added dark mode

--- index.js
+++ index.js
@@ -2,11 +2,23 @@
 let themeButton = document.getElementById("theme-button");
 
 // TODO: Complete the toggleDarkMode function
-const toggleDarkMode () => {
+const toggleDarkMode = () => {
     document.body.classList.toggle("dark-mode");
-    console.log("Clicked dark mode!");
+    document.a.classList.toggle("dark-mode");
+    let themeButton2 = document.getElementById("theme-button");
+    themeButton2.classList.toggle("theme-button-light-mode");
 
+    //toggling the text in the button
+    if (themeButton2.innerHTML === "LIGHT MODE" ){
+        themeButton2.textContent = "DARK MODE";
+    } else{
+    themeButton2.textContent = "LIGHT MODE";
+    }
+
+    /*console.log("Clicked dark mode!");*/
+3
 }
+
 
 // TODO: Register a 'click' event listener for the theme button
 // Set toggleDarkMode as the callback function.
